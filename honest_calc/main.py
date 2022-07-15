531f0aad102de33c1de56d2f7ead36dadc50ce1c
blix95 <(I5hcB@t9l!>
2022-07-15 16:39:57 +0200

    added calculations process

--- honest_calc/main.py
+++ honest_calc/main.py
@@ -4,6 +4,8 @@ msg_1 = "Do you even know what numbers are? Stay focused!"
 
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
+msg_3 = "Yeah... division by zero. Smart move..."
+
 
 def isfloat(n): #funkcja sprawdzajaca czy liczba jest typu float
     try:
@@ -16,8 +18,28 @@ while True:
     calc = input()
     x, oper, y = calc.split() #rozdzielanie wpisanego tekstu
     if (x.isnumeric() or isfloat(x)) and (y.isnumeric() or isfloat(y)): # sprawdzanie czy wpisane wartosci są liczbami
+        x = float(x)
+        y = float(y)
         if oper == "+" or oper == "-" or oper == "*" or oper == "/": #sprawdza czy wprowadzony operator jest poprawny
-            break
+            if oper == "+":
+                result = x + y
+                print(result)
+                break
+            elif oper == "-":
+                result = x - y
+                print(result)
+                break
+            elif oper == "*":
+                result = x * y
+                print(result)
+                break
+            elif oper == "/" and y != 0:
+                result = x / y
+                print(result)
+                break
+            else:
+                print(msg_3)
+                continue
         else:
             print(msg_2)
             continue
